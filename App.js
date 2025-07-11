a720bc242b0442b005afe99d9eafe4e05b9a8637
Aman Uniyal <your-email@amanuniyal315@gmail.com>
2025-07-11 20:43:47 +0530

    📁 Episode 01 and Episode 02 added

--- App.js
+++ App.js
@@ -15,15 +15,17 @@
     //     </div>
     // </div>
 
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 const parent = React.createElement("div", { id: "parent" },
 
     [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I am h1 tag"),
+        [React.createElement("h1", {}, "This is Namaste react🚀🚀"),
 
-        React.createElement("h2", {}, "I am h2 tag")]),
+        React.createElement("h2", {}, "Ammu Uniyal")]),
     React.createElement("div", { id: "child-2" },
-        [React.createElement("h1", {}, "I am h1 tag"),
+        [React.createElement("h1", {}, "Jai Shree Ram"),
 
         React.createElement("h2", {}, "I am h2 tag")])
     ]);
